7800035e8b75a5bb53329ef19d8329fc6027eb72
Gabriel Souza <gabrielsouzaa@live.com>
2019-09-24 19:16:16 -0300

    add swagger config and kafka producer

--- src/main/java/com/reciclatown/controller/OrdemManutencaoController.java
+++ src/main/java/com/reciclatown/controller/OrdemManutencaoController.java
@@ -1,39 +1,54 @@
 package com.reciclatown.controller;
 
+import com.reciclatown.kafka.ProducerKafka;
 import com.reciclatown.model.OrdemManutencao;
 import com.reciclatown.repository.OrdemManutencaoRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
+@Api(value = "OrdensManutencao")
 public class OrdemManutencaoController {
 
     @Autowired
     OrdemManutencaoRepository repository;
 
+    @Value("${kafka.bootstrap.servers}")
+    String bootstrapServer;
+
+    @ApiOperation(value = "Buscar ordens de manutencao")
     @GetMapping(value = "/ordens_manutencao", produces = MediaType.APPLICATION_JSON_VALUE)
     public Iterable<OrdemManutencao> buscarTodasOrdensManutencaoes() {
         return repository.findAll();
     }
 
+    @ApiOperation(value = "Criar nova ordem de manutencao")
     @PostMapping(value = "/ordens_manutencao", consumes = MediaType.APPLICATION_JSON_VALUE)
     public OrdemManutencao cadastrarNovaOrdemManutencao(@RequestBody OrdemManutencao ordemManutencao) {
         return repository.save(ordemManutencao);
     }
 
+    @ApiOperation(value = "Buscar informacoes de uma ordem de manutencao")
     @GetMapping(value = "/ordens_manutencao/{id}")
     public Optional<OrdemManutencao> buscarOrdemManutencaoPeloId(@PathVariable String id) {
         return repository.findById(id);
     }
 
-    @PutMapping(value = "/ordens_manutencao/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Atualizar dados de uma ordem de manutencao")
+    @PutMapping(value = "/ordens_manutencao", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void atualizarDadosDaOrdemManutencao(@RequestBody OrdemManutencao ordemManutencao) {
+        ProducerKafka producerKafka = new ProducerKafka();
+        producerKafka.enviarEventoKafka(bootstrapServer, ordemManutencao);
         repository.save(ordemManutencao);
     }
 
+    @ApiOperation(value = "Excluir uma ordem de manutencao")
     @DeleteMapping(value = "/ordens_manutencao/{id}")
     public void excluirOrdemManutencao(@PathVariable String id) {
         OrdemManutencao st = new OrdemManutencao();
---
+++ src/main/java/com/reciclatown/kafka/ProducerKafka.java
@@ -0,0 +1,35 @@
+package com.reciclatown.kafka;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.reciclatown.model.OrdemManutencao;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+
+@Component
+public class ProducerKafka {
+
+    public void enviarEventoKafka(String bootstrapServer, OrdemManutencao ordemManutencao) {
+        Properties producerProperties = new Properties();
+        producerProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        producerProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producerProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        KafkaProducer<String, String> myProducer = new KafkaProducer<>(producerProperties);
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            String ordemAtualizada = mapper.writeValueAsString(ordemManutencao);
+            ProducerRecord record = new ProducerRecord<>("ordens-manutencao", ordemAtualizada);
+			myProducer.send(record);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            myProducer.close();
+        }
+    }
+
+}

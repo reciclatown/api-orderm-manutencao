c8b3d609c1792d80ab510d95f44088d3530fa80a
Gabriel Souza <gabrielsouzaa@live.com>
2019-09-26 01:53:15 -0300

    add cors config

--- src/main/java/com/reciclatown/controller/OrdemManutencaoController.java
+++ src/main/java/com/reciclatown/controller/OrdemManutencaoController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin
 @Api(value = "OrdensManutencao")
 public class OrdemManutencaoController {
 
